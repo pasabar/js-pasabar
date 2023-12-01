dfcbeb7ee9495f5087744f1576c96e68070aa5fa
fahad17 <fahadra96@gmail.com>
2023-12-02 00:01:12 +0700

    Refactor import statements and fix formatting issues

--- js/config/config.js
+++ js/config/config.js
@@ -1,30 +1,31 @@
-import { setCookieWithExpireHour } from 'https://jscroot.github.io/cookie/croot.js';
+import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
 //token
 export function getTokenFromAPI() {
-  const tokenUrl = "https://asia-southeast2-pasabar.cloudfunctions.net/Admin-Login";
+  const tokenUrl =
+    "https://asia-southeast2-pasabar.cloudfunctions.net/Admin-Login";
   fetch(tokenUrl)
-    .then(response => response.json())
-    .then(tokenData => {
+    .then((response) => response.json())
+    .then((tokenData) => {
       if (tokenData.token) {
         userToken = tokenData.token;
-        console.log('Token dari API:', userToken);
+        console.log("Token dari API:", userToken);
       }
     })
-    .catch(error => console.error('Gagal mengambil token:', error));
+    .catch((error) => console.error("Gagal mengambil token:", error));
 }
-export function GetDataForm(){
-            const email = document.querySelector("#email").value;
-            const password = document.querySelector("#password").value;
-            const role = document.querySelector("#role").value;
-            console.log(password)
+export function GetDataForm() {
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+  const role = document.querySelector("#role").value;
+  console.log(password);
 
-            const data = {
-                email: email,
-                password: password,
-                role: role
-            };
-            return data
+  const data = {
+    email: email,
+    password: password,
+    role: role,
+  };
+  return data;
 }
 //login
 export function PostLogin() {
@@ -35,36 +36,33 @@ export function PostLogin() {
   const data = {
     email: email,
     password: password,
-    role: role
+    role: role,
   };
   return data;
 }
 
-
-
 function ResponsePostLogin(response) {
   if (response && response.token) {
-    console.log('Token User:', response.token);
-    setCookieWithExpireHour('user_token', response.token, 2);
-    window.location.href = 'https://pasabar.github.io/Dashboard/';
+    console.log("Token User:", response.token);
+    setCookieWithExpireHour("user_token", response.token, 2);
+    window.location.href = "https://pasabar.my.id/Dashboard/";
     Swal.fire({
-      icon: 'success',
-      title: 'Login Successful',
-      text: 'You have successfully logged in!',
+      icon: "success",
+      title: "Login Successful",
+      text: "You have successfully logged in!",
     });
   } else {
     Swal.fire({
-      icon: 'error',
-      title: 'Login Failed',
-      text: 'Invalid email or password. Please try again.',
+      icon: "error",
+      title: "Login Failed",
+      text: "Invalid email or password. Please try again.",
     });
   }
 }
 
-
 export function ResponsePost(result) {
-    AlertPost(result);
+  AlertPost(result);
 }
 export function ResponseLogin(result) {
-  ResponsePostLogin(result)
+  ResponsePostLogin(result);
 }
