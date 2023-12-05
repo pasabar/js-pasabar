4046a222dc88af7a7b6fe284412b3406d9e2a233
fahad17 <fahadra96@gmail.com>
2023-12-06 01:01:14 +0700

    Refactor code for getting and deleting catalog data

--- js/controller/cont_catalog.js
+++ js/controller/cont_catalog.js
@@ -1,171 +1,163 @@
 const getTokenFromCookies = (cookieName) => {
-  const cookies = document.cookie.split(";");
+  const cookies = document.cookie.split(';')
   for (const cookie of cookies) {
-    const [name, value] = cookie.trim().split("=");
+    const [name, value] = cookie.trim().split('=')
     if (name === cookieName) {
-      return value;
+      return value
     }
   }
-  return null;
-};
+  return null
+}
 
 const getAllCatalog = async () => {
-  const token = getTokenFromCookies("Login");
+  const token = getTokenFromCookies('Login')
 
   if (!token) {
     Swal.fire({
-      icon: "warning",
-      title: "Authentication Error",
-      text: "You are not logged in.",
+      icon: 'warning',
+      title: 'Authentication Error',
+      text: 'You are not logged in.',
     }).then(() => {
-      window.location.href = "https://pasabar.my.id/pages/signin.html";
-    });
-    return;
+      window.location.href = 'https://pasabar.my.id/pages/signin.html'
+    })
+    return
   }
 
-  const targetURL =
-    "https://asia-southeast2-pasabar.cloudfunctions.net/GetAll-dataCatalog";
+  const targetURL = 'https://asia-southeast2-pasabar.cloudfunctions.net/GetAll-dataCatalog'
 
-  const myHeaders = new Headers();
-  myHeaders.append("Login", token);
+  const myHeaders = new Headers()
+  myHeaders.append('Login', token)
 
   const requestOptions = {
-    method: "GET",
+    method: 'GET',
     headers: myHeaders,
-    redirect: "follow",
-  };
+    redirect: 'follow',
+  }
 
   try {
-    const response = await fetch(targetURL, requestOptions);
-    const data = await response.json();
+    const response = await fetch(targetURL, requestOptions)
+    const data = await response.json()
 
     if (data.status === true) {
-      displayCatalogData(data.data, "CatalogDataBody");
+      displayCatalogData(data.data, 'CatalogDataBody')
     } else {
       Swal.fire({
-        icon: "error",
-        title: "Error",
+        icon: 'error',
+        title: 'Error',
         text: data.message,
-      });
+      })
     }
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error)
   }
-};
+}
 
 const deleteCatalog = async (nomorId) => {
-  const token = getTokenFromCookies("Login");
+  const token = getTokenFromCookies('Login')
 
   if (!token) {
-    showAlert("Header Login Not Found", "error");
-    return;
+    showAlert('Header Login Not Found', 'error')
+    return
   }
 
-  const targetURL =
-    "https://asia-southeast2-pasabar.cloudfunctions.net/Delete-Catalog";
+  const targetURL = 'https://asia-southeast2-pasabar.cloudfunctions.net/Delete-Catalog'
 
-  const myHeaders = new Headers();
-  myHeaders.append("Login", token);
-  myHeaders.append("Content-Type", "application/json");
+  const myHeaders = new Headers()
+  myHeaders.append('Login', token)
+  myHeaders.append('Content-Type', 'application/json')
 
   const requestOptions = {
-    method: "DELETE",
+    method: 'DELETE',
     headers: myHeaders,
     body: JSON.stringify({ nomorid: nomorId }),
-    redirect: "follow",
-  };
+    redirect: 'follow',
+  }
 
   try {
-    const response = await fetch(targetURL, requestOptions);
-    const data = await response.json();
+    const response = await fetch(targetURL, requestOptions)
+    const data = await response.json()
 
     if (data.status === 200) {
       Swal.fire({
-        icon: "success",
-        title: "Success",
-        text: "catalog deleted successfully!",
+        icon: 'success',
+        title: 'Success',
+        text: 'catalog deleted successfully!',
       }).then(() => {
-        getAllEmployees();
-      });
+        getAllEmployees()
+      })
     } else {
       Swal.fire({
-        icon: "error",
-        title: "Error",
+        icon: 'error',
+        title: 'Error',
         text: data.message,
-      });
+      })
     }
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error)
   }
-};
+}
 
 // Function to handle the delete confirmation
 const deleteCatalogHandler = (nomorId) => {
   Swal.fire({
-    title: "Are you sure?",
+    title: 'Are you sure?',
     text: "You won't be able to revert this!",
-    icon: "warning",
+    icon: 'warning',
     showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it!",
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!',
   }).then((result) => {
     if (result.isConfirmed) {
-      deleteCatalog(nomorId);
+      deleteCatalog(nomorId)
     }
-  });
-};
+  })
+}
 
 const editCatalog = (nomorId) => {
-  window.location.href = `formedit_catalog.html?nomorid=${nomorId}`;
-};
+  window.location.href = `formedit_catalog.html?nomorid=${nomorId}`
+}
 // Event listener to handle clicks on the table
-document
-  .getElementById("CatalogDataBody")
-  .addEventListener("click", (event) => {
-    const target = event.target;
-    if (target.classList.contains("edit-link")) {
-      const nomorId = parseInt(target.getAttribute("data-nomorid"));
-      editCatalog(nomorId);
-    } else if (target.classList.contains("delete-link")) {
-      const nomorId = parseInt(target.getAttribute("data-nomorid"));
-      deleteCatalogHandler(nomorId);
-    }
-  });
+document.getElementById('CatalogDataBody').addEventListener('click', (event) => {
+  const target = event.target
+  if (target.classList.contains('edit-link')) {
+    const nomorId = parseInt(target.getAttribute('data-nomorid'))
+    editCatalog(nomorId)
+  } else if (target.classList.contains('delete-link')) {
+    const nomorId = parseInt(target.getAttribute('data-nomorid'))
+    deleteCatalogHandler(nomorId)
+  }
+})
 
 const displayCatalogData = (catalogData, tableBodyId) => {
-  const catalogDataBody = document.getElementById(tableBodyId);
+  const catalogDataBody = document.getElementById(tableBodyId)
 
-  catalogDataBody.innerHTML = "";
+  catalogDataBody.innerHTML = ''
 
   if (catalogData && catalogData.length > 0) {
     catalogData.forEach((item) => {
-      const newRow = document.createElement("tr");
+      const newRow = document.createElement('tr')
       newRow.innerHTML = `
         <td class="px-4 py-3">${item.nomorid}</td>
         <td class="px-4 py-3">${item.title}</td>
         <td class="px-4 py-3">${item.description}</td>
         <td class="px-4 py-3">${item.lokasi}</td>
         <td class="px-4 py-3">
-          <img src="${
-            item.image
-          }" alt="Catalog Image" style="max-width: 100px; max-height: 100px;">
+          <img src="${item.image}" alt="Catalog Image" style="max-width: 100px; max-height: 100px;">
         </td>
-        <td class="px-4 py-3">${item.status ? "Active" : "Inactive"}</td>
+        <td class="px-4 py-3">${item.status ? 'Active' : 'Inactive'}</td>
         <td class="px-4 py-3">
           <a href="#" class="edit-link" data-nomorid="${item.nomorid}">Edit</a>
-          <a href="#" class="delete-link" data-nomorid="${
-            item.nomorid
-          }">Delete</a>
+          <a href="#" class="delete-link" data-nomorid="${item.nomorid}">Delete</a>
         </td>
-      `;
+      `
 
-      catalogDataBody.appendChild(newRow);
-    });
+      catalogDataBody.appendChild(newRow)
+    })
   } else {
-    catalogDataBody.innerHTML = `<tr><td colspan="6">No catalog data found.</td></tr>`;
+    catalogDataBody.innerHTML = `<tr><td colspan="6">No catalog data found.</td></tr>`
   }
-};
+}
 
 // Initial fetch of all catalogs
-getAllCatalog();
+getAllCatalog()
